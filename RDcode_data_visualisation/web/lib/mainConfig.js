b5fad8c66397ee76321cf9176e0446c13a08d88b
cybigot <cyril.bigot.pro@gmail.com>
2021-04-26 18:26:38 +0200

    dataviz update

--- RDcode_data_visualisation/web/lib/mainConfig.js
+++ RDcode_data_visualisation/web/lib/mainConfig.js
@@ -1,5 +1,5 @@
-const baseUrl = "https://api.orphacode.org/"; // RDcode API url
-//const baseUrl = "http://localhost:8080/"; // RDcode API url
+//const baseUrl = "https://api.orphacode.org/"; // RDcode API url
+const baseUrl = "http://localhost:8080/"; // RDcode API url
 const suggestDelay = 500; // time in ms needed to autolaunch the suggest search
 var paginationSize = 5; // number of lines displayed for the search
 var tooltip = {};
